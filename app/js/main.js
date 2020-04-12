6a618ec3d47a5c6691f094175a46f8445dcf3901
Alex-project1 <kiriluka68@gmail.com>
2020-04-12 21:58:20 +0300

    added slider 80%

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        readOnly: true,
+        starWidth: "12px"
+    });
+
+
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
 
 
 });
