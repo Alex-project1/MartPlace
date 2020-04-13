7a4ed611b047b4ab0e64a2871ecc003dfdccab90
Alex-project1 <kiriluka68@gmail.com>
2020-04-13 17:56:25 +0300

    need icon

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,11 @@ $(function() {
     });
 
 
-    $('.slider__inner').slick({
+    $('.slider__items').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
+        prevArrow: '<button class="slick-prev slick-arrow"></button>',
+        nextArrow: '<button class="slick-next slick-arrow"></button>',
     });
 
 
